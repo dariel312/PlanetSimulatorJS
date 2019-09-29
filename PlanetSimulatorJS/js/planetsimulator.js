053209729dcf4072a194266ea66bfcd7460c1cdc
Dariel Barroso <dariel_games@yahoo.com>
2019-09-29 16:17:31 -0400

    lots of updates

---
+++ PlanetSimulatorJS/js/planetsimulator.js
@@ -0,0 +1,161 @@
+﻿function PlanetSimulator(boundX, boundY, numPlanets) {
+    var system = [];
+
+    this.System = system;
+
+    //create planets
+
+    var god = new Planet(new Vector(boundX/2, boundY / 2), 30);
+    god.Velocity = new Vector(5, 0);
+    god.Color = new Vector(255, 225, 0);
+    this.God = god;
+    system.push(god);
+
+
+    for (var i = 1; i <= numPlanets; i++) {
+        var r = Math.ceil(15 / (1+i) + 0.2* Math.sqrt(i));
+        var pos = new Vector(rand(boundX)+boundX/2, rand(boundY)+boundY/2);
+        var speed = rand(100)/r/r;
+        var dir = rand(360) * 2 * Math.PI / 360;
+        var vel = new Vector(speed * Math.cos(dir), speed * Math.sin(dir));
+        var planet = new Planet(pos, r, vel);
+        system.push(planet);
+    }
+    var n = 0;
+
+    this.Update = function (deltaTime) {
+        var deadPlanets = [];
+
+        if (n < numPlanets) {
+            n++;
+        }
+
+        //Calculate Force->Acceleration->Movement
+        for (var i = 0; i < system.length; i++) {
+            var xForce = 0;
+            var yForce = 0;
+
+            for (var n = 0; n < system.length; n++) {
+                if (n != i) {
+                    distance = dist(system[n].Pos.x, system[n].Pos.y, system[i].Pos.x, system[i].Pos.y);
+                    if (distance >= 1) {
+                        var force = system[n].Mass() * system[i].Mass() / (Math.pow(distance, 2));
+                        var angle = Math.atan2(system[n].Pos.y - system[i].Pos.y, system[n].Pos.x - system[i].Pos.x);
+                        xForce += force * Math.cos(angle);
+                        yForce += force * Math.sin(angle);
+                    }
+                }
+            }
+            system[i].Force = new Vector(xForce, yForce);
+            system[i].Update(deltaTime);
+        }
+        //Check if there was a collision (note: this is done after all movement is made)
+        for (var i = 0; i < system.length; i++) {
+            for (var n = 0; n < system.length; n++) {
+                if (n != i) {
+                    if (dist(system[n].Pos.x, system[n].Pos.y, 0, system[i].Pos.x, system[i].Pos.y, 0) <= system[i].Radius + system[n].Radius) {
+
+                        var a = 0;
+                        var b = 0;
+
+                        if (system[i].Radius >= system[n].Radius) {
+                            a = i;
+                            b = n;
+                        }
+                        else {
+                            a = n;
+                            b = i;
+                        }
+
+                        system[a].Radius = Math.cbrt((system[a].Mass() + system[b].Mass()) * 3 / 4 / Math.PI);
+                        system[b].Radius = 0;
+                        deadPlanets.push(b);
+
+                        var force = system[b].Mass() * system[b].Acceleration();
+                        var angle = Math.atan2(system[b].Pos.y - system[a].Pos.y, system[b].Pos.x - system[a].Pos.x);
+                        var vectorForce = new Vector(force * Math.cos(angle), force * Math.sin(angle));
+                        system[a].Force.Add(vectorForce);
+
+                    }
+                }
+            }
+        }
+
+        //delete dead planets
+        deadPlanets.forEach(x => system.splice(x, 1));
+
+    };
+
+}
+
+function Vector(xValue, yValue, zValue) {
+    this.x = xValue;
+    this.y = yValue;
+    this.z = zValue;
+
+    this.Add = function (vector) {
+        this.x += vector.x;
+        this.y += vector.y;
+        this.z += vector.z;
+        return this;
+    };
+    this.Subtract = function (vector) {
+        this.x -= vector.x;
+        this.y -= vector.y;
+        this.z -= vector.z;
+        return this;
+    };
+    this.Scale = function (value) {
+        this.x *= value;
+        this.y *= value;
+        this.z *= value;
+        return this;
+    };
+}
+
+function Planet(position, radius, velocity) {
+    this.Pos = position;
+    this.Radius = radius;
+    this.Velocity = velocity;
+    //this.Color = color(rand(155) + 100, rand(155) + 100, rand(155) + 100);
+    this.Color = new Vector(rand(155) + 100, rand(155) + 100, rand(155) + 100);
+    this.Force = new Vector(0, 0);
+
+   
+
+    this.Mass = function () {
+        return Math.PI * 4 / 3 * Math.pow(this.Radius, 3);
+    };
+    this.Acceleration = function () {
+        return new Vector(this.Force.x / this.Mass(), this.Force.y / this.Mass());
+    };
+    this.Update = function (deltaTime) {
+        this.Velocity.Add(this.Acceleration().Scale(deltaTime));
+        this.Pos.x += this.Velocity.x * deltaTime;
+        this.Pos.y += this.Velocity.y * deltaTime;
+
+        //Collision check
+        //if (this.Pos.x > screenWidth) {
+        //    this.Velocity.x = 0;
+        //    this.Pos.x = screenWidth;
+        //}
+        //if (this.Pos.x < 0) {
+        //    this.Velocity.x = 0;
+        //    this.Pos.x = 0;
+        //}
+
+        //if (this.Pos.y > screenHeight) {
+        //    this.Velocity.y = 0;
+        //    this.Pos.y = screenHeight;
+        //}
+        //if (this.Pos.y < 0) {
+        //    this.Velocity.y = 0;
+        //    this.Pos.y = 0;
+        //}
+    };
+
+}
+
+function rand(num) {
+    return Math.floor(Math.random() * num);
+}
--- PlanetSimulatorJS/js/scripts.js
+++ PlanetSimulatorJS/js/scripts.js
@@ -1,177 +1,71 @@
 /// <reference path="jquery-3.1.0.js" />
 /// <reference path="p5.js" />
 
-var screenWidth = 1820;
-var screenHeight = 800;
-var system = [];
+var screenWidth = 600;
+var screenHeight = 600;
 var lastFrameTime = 0;
-var deltaTime = 0;
-var timeSpeed = 1; //1 = 100%
+var timeSpeed = 10; //1 = 100%
+var isTouching = false;
+var cam = new Vector(300, 300, 1);
+var camOffset = null;
+var sim = null; 
 
-function setup(){
- 	createCanvas(screenWidth, screenHeight);
- 	background(0);
-    
- 	var r = 0;
-	var god = new Planet(50);
-	god.Velocity = new Vector(20,0);
-	god.Pos = new Vector(0, screenHeight/2);
-	god.Color = color(255, 225,0);
-	
-	system.push(god);
-	for(var i=1; i <= 70; i++){
-	    system.push(new Planet(r));
-	    r = Math.ceil(20/ (i) + 0.2*i);
-	    //r = 50 - (5 * i);
-	}
+function setup() {
+    createCanvas(screenWidth, screenHeight);
+    background(0);
+    sim = new PlanetSimulator(screenWidth/8 , screenHeight/8, 200);
 }
-
 function draw() {
     //update Delta Time
-    deltaTime = timeSpeed * (millis() - lastFrameTime)/1000;
+    var deltaTime = timeSpeed * (millis() - lastFrameTime) / 1000;
 
     //Perform calculations
-    update();
+    sim.Update(deltaTime);
+    camOffset = new Vector(-screenWidth / 2, -screenHeight / 2);
+    cam = camOffset.Add(sim.God.Pos);
 
     //Draw background
     fill(0, 0, 0);
     rect(0, 0, screenWidth, screenHeight);
 
     //Draw all planets
-    for (var i = 0; i < system.length; i++) {
-        system[i].Draw();
+    for (var i = 0; i < sim.System.length; i++) {
+        drawPlanet(sim.System[i]);
     }
+
+    console.log("Num planets: " + sim.System.length);
 
     //update lastFrameTime
     lastFrameTime = millis();
 }
-function update() {
 
-    //Calculate Force->Acceleration->Movement
-    for (var i = 0; i < system.length; i++) {
-        var xForce = 0;
-        var yForce = 0;
-
-        for (var n = 0; n < system.length; n++) {
-            if (n != i) {
-
-                distance = dist(system[n].Pos.x, system[n].Pos.y, system[i].Pos.x, system[i].Pos.y);
-                if (distance >= 1) {
-                    var force = system[n].Mass() * system[i].Mass() / (Math.pow(distance, 2));
-                    var angle = Math.atan2(system[n].Pos.y - system[i].Pos.y, system[n].Pos.x - system[i].Pos.x);
-                    xForce += force * Math.cos(angle);
-                    yForce += force * Math.sin(angle);
-                }
-            }
-        }
-        system[i].Force = new Vector(xForce, yForce);
-        system[i].Update();
-    }
-    //Check if there was a collision (note: this is done after all movement is made)
-    for (var i = 0; i < system.length; i++) {
-        for (var n = 0; n < system.length; n++) {
-            if (n != i) {
-                if (dist(system[n].Pos.x, system[n].Pos.y, 0, system[i].Pos.x, system[i].Pos.y, 0) <= system[i].Radius + system[n].Radius) {
-                   
-					var a = 0;
-					var b = 0;
-					
-					if(system[i].Radius >= system[n].Radius){
-						a = i;
-						b = n;
-					}
-					else {
-						a = n;
-						b = i;
-					}
-					
-					system[a].Radius = Math.cbrt(4 * (system[a].Mass() + system[b].Mass()) / 3 / Math.PI);
-                    system[b].Radius = 0;
-					
-					var force = system[b].Mass()*system[b].Acceleration();
-					var angle = Math.atan2(system[b].Pos.y - system[a].Pos.y, system[b].Pos.x - system[a].Pos.x);
-                    var vectorForce = new Vector(force * Math.cos(angle), force * Math.sin(angle));
-					system[a].Force.Add(vectorForce);
-					
-                }
-            }
-        }
+function drawPlanet(planet) {
+    //make hidden property to store p5 color, kinda ugly
+    if (planet.$color === undefined) {
+        planet.$color = color(planet.Color.x, planet.Color.y, planet.Color.z);
     }
 
+    fill(planet.$color);
+    ellipse(planet.Pos.x - cam.x, planet.Pos.y - cam.y, planet.Radius * 2, planet.Radius * 2);
 }
 
 var frame = false;
-function touchEnded() {
+function touchEnded(event) {
+    console.log(event);
     if (frame == false) {
-        timeSpeed = 10;
+        timeSpeed = 20;
         frame = true;
     }
     else {
-        timeSpeed = 10;
+        timeSpeed = 1;
         frame = false;
     }
 }
+function touchStarted(event) {
+    console.log(event);
 
-//Classes
-function Vector(xValue, yValue) {
-    this.x = xValue;
-    this.y = yValue;
-	this.Add = function (vector){
-		this.x += vector.x;
-		this.y += vector.y;
-		return this;
-	};
-	this.Subtract = function (vector){
-		this.x -= vector.x;
-		this.y -= vector.y;
-		return this;
-	};
-	this.Scale = function (value){
-		this.x *= value;
-		this.y *= value;
-		return this;
-	};
+    isTouching = true;
 }
-function Planet(radius){
-	this.Pos = new Vector(random(screenWidth), random(screenHeight));
-	this.Radius = radius;
-	this.Color = color(random(155)+100, random(155)+100, random(155)+100);
-	this.Force = new Vector(0, 0);
-	this.Velocity = new Vector(random(60)-30,random(60)-30);
-	
-	this.Mass = function () {
-	    return Math.PI * 3 / 4 * this.Radius * this.Radius * this.Radius; 
-	};
-	this.Acceleration = function () {
-	    return new Vector(this.Force.x / this.Mass(), this.Force.y / this.Mass());
-	};
-	this.Update = function () {
-		this.Velocity.Add(this.Acceleration().Scale(deltaTime));
-	    this.Pos.x += this.Velocity.x*deltaTime;
-	    this.Pos.y += this.Velocity.y*deltaTime;
 
-	    //Collision check
-	    if (this.Pos.x > screenWidth){
-	        this.Velocity.x = 0;
-	        this.Pos.x = screenWidth;
-	    }
-	    if (this.Pos.x < 0) {
-	        this.Velocity.x = 0;
-	        this.Pos.x = 0;
-	    }
 
-	    if (this.Pos.y > screenHeight) {
-	        this.Velocity.y = 0;
-	        this.Pos.y = screenHeight;
-	    }
-	    if (this.Pos.y < 0) {
-	        this.Velocity.y = 0;
-	        this.Pos.y = 0;
-	    }
-	};
-	this.Draw = function() {
-		fill(this.Color);
-		ellipse(this.Pos.x, this.Pos.y, this.Radius * 2, this.Radius * 2);
-	};
-}
 
